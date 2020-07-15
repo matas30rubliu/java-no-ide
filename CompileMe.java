aa7dedc3b398babe3b7a9a389ba139efb1bc01d0
mlil S340 <matas30rubliu@gmail.com>
2020-07-15 15:45:01 +0300

    Test compilation

---
+++ CompileMe.java
@@ -0,0 +1,18 @@
+package training.basics;
+
+/**
+ * <ul>
+ *   <li> Compile: javac -d ./target CompileMe.java </li>
+ *   <li> Run:     java -cp ./target training.basics.CompileMe </li>
+ * </ul>
+ * <b> Generate this Javadoc: javadoc -d ./target CompileMe.java </b>
+ * @since 2020-07-15
+ */
+public class CompileMe {
+	public static void main(String[] args) {
+		System.out.println("args received:");
+		for (String arg : args) {
+			System.out.println(arg);
+		}
+	}
+}
