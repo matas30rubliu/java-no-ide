fbb1e874d215d6c967d7b8faf62e38e365e5c33c
matas30rubliu <matas30rubliu@gmail.com>
2020-07-16 18:04:32 +0300

    Completed Fibonacci & Negafibonacci

--- FibonacciCalculator.java
+++ FibonacciCalculator.java
@@ -9,5 +9,23 @@ public class FibonacciCalculator {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         System.out.println(String.format("Calculating F(%s)...", n));
+        int answer = findFib(n);
+        System.out.println(String.format("F(%s)=%s", n, answer));
+    }
+
+    private static int findFib(int n) {
+        int answer = 0;
+        // F(-2)=-1, F(-1)=1
+        if (n == -1 || n == 1) {
+            return 1;
+        }
+        if (n == 0) {
+            return 0;
+        }
+        if (n > 0) {
+            return findFib(n - 1) + findFib(n - 2);
+        }
+        // n < 0, Negafibonacci
+        return findFib(n + 2) - findFib(n + 1);
     }
 }
