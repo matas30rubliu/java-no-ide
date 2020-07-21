8c3482afb72080b9be336d717eeae85759e734a3
matas30rubliu <matas30rubliu@gmail.com>
2020-07-21 11:03:32 +0300

    Add problem description

--- assessments/AmazonAssessment.java
+++ assessments/AmazonAssessment.java
@@ -4,6 +4,15 @@ public class AmazonAssessment {
 
     public static void main(String[] args) {
         System.out.println("Test ID = 23280666561652");
+        int flightDuration;
+        
+        // List of durations of N movies
+        // Each movie is assigned a unique ID, numbered from 0 to N-1
+        ArrayList<Integer> movieDurations;
+        
+        // Passangers watch exactly two movies and no movie is played twice 
+        // If there are multiple combinations of movies, select the combination with the longest movie
+        // Write an algorithm to output a pair of movie IDs that finish exactly 30 mins before plane lands
     }
 
 }
