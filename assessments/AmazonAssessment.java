3b1cb4d4aea8a019c2fab577230e05c025a03eb6
mlil S340 <matas30rubliu@gmail.com>
2020-07-23 00:14:56 +0300

    Continue with task 1

--- assessments/AmazonAssessment.java
+++ assessments/AmazonAssessment.java
@@ -1,9 +1,14 @@
 package training.assessments;
 
+import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.Iterator;
+import training.exercises.utils.CliParser;
 
 /**
- * root_dir>javac -d ./target assessments/AmazonAssessment.java
+ * root_dir>javac -d ./target utils/CliParser.java assessments/AmazonAssessment.java
  * root_dir>java -cp ./target training.assessments.AmazonAssessment
  */
 public class AmazonAssessment {
@@ -11,15 +16,46 @@ public class AmazonAssessment {
     public static void main(String[] args) {
         System.out.println("Test ID = 23280666561652");
         int flightDuration;
-        
+
+        System.out.println("Enter space-separated movie durations: ");
         // List of durations of N movies
         // Each movie is assigned a unique ID, numbered from 0 to N-1
-        ArrayList<Integer> movieDurations;
+
+        // I really want the ArrayList here!
+        ArrayList<Integer> movieDurations = new ArrayList<Integer>(
+            // this will return List
+            Arrays.stream(CliParser.parseIntArray())
+                .boxed()
+                .collect(Collectors.toList()
+            )
+        );
 
         // Passangers watch exactly two movies and no movie is played twice 
         // If there are multiple combinations of movies, select the combination with the longest movie
         // Write an algorithm to output a pair of movie IDs that finish exactly 30 mins before plane lands
         Pair a = new Pair(Integer.valueOf(1), Integer.valueOf(1));
+        
+        int x1 = findLargestTimeAndRemove(movieDurations);
+        int x2 = findLargestTimeAndRemove(movieDurations);
+        System.out.println(x1);
+        System.out.println(x2);
+    }
+
+    private static int findLargestTimeAndRemove(ArrayList<Integer> times) {
+        System.out.println("findLargestTimeAndRemove>>>");
+        int maxTime = 0;
+        Iterator<Integer> iter = times.iterator();
+        int index = 1;
+        while(iter.hasNext()) {
+            Integer time = iter.next();
+            System.out.println("[" + index + "] = " + time);
+            if (time > maxTime) {
+                maxTime = time;
+                iter.remove();
+            }
+            index++;
+        }
+        return maxTime;
     }
 
     static class Pair {
