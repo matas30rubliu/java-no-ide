3e486add6f7ec2e1b071a9158bd0a53789c3f78e
matas30rubliu <matas30rubliu@gmail.com>
2020-07-22 18:16:46 +0300

    First assessment task1

--- assessments/AmazonAssessment.java
+++ assessments/AmazonAssessment.java
@@ -1,5 +1,11 @@
 package training.assessments;
 
+import java.util.ArrayList;
+
+/**
+ * root_dir>javac -d ./target assessments/AmazonAssessment.java
+ * root_dir>java -cp ./target training.assessments.AmazonAssessment
+ */
 public class AmazonAssessment {
 
     public static void main(String[] args) {
@@ -9,10 +15,25 @@ public class AmazonAssessment {
         // List of durations of N movies
         // Each movie is assigned a unique ID, numbered from 0 to N-1
         ArrayList<Integer> movieDurations;
-        
+
         // Passangers watch exactly two movies and no movie is played twice 
         // If there are multiple combinations of movies, select the combination with the longest movie
         // Write an algorithm to output a pair of movie IDs that finish exactly 30 mins before plane lands
+        Pair a = new Pair(Integer.valueOf(1), Integer.valueOf(1));
     }
 
+    static class Pair {
+        Integer first;
+        Integer second;
+
+        public Pair(Integer first, Integer second) {
+            this.first = first;
+            this.second = second;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("<%s,%s>", this.first, this.second);
+        }
+    }
 }
